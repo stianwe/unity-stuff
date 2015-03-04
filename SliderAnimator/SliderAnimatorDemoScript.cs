0825c27f2bf774e11a66ca8c88ea63ac250cbdb5
Stian Weie <stianwe@gmail.com>
2015-03-04 13:00:22 +0100

    Add demo script to show how SliderAnimator can be used

---
+++ SliderAnimator/SliderAnimatorDemoScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SliderAnimatorDemoScript : MonoBehaviour {
+
+	void Update () {
+        // Animate the slider when releasing the f key
+        if (Input.GetKeyUp("f"))
+        {
+            // Retrieve the animator script from the slider which it is attached to (named simply "Slider").
+            var animator = GameObject.Find("Slider").GetComponent<SliderAnimator>();
+            // Set the wrap around action to print a single message on slider wrap around.
+            animator.OnWrapAround = () => { Debug.Log("Slider wrapped around!"); };
+            // Set the end action to print a single message when the slider animation ends.
+            animator.OnEnd = () => { Debug.Log("Slider animation ended!"); };
+            // Start the slider animation at 30%, end it at 40%, and make the animation decrement (go from right to left)
+            animator.Animate(0.3f, 0.4f, false);
+        }
+	}
+}
